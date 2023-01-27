4745b69ea77ab2e440d92c144f852de6c9a9930b
hem193 <hemanthcastel@gmail.com>
2023-01-27 17:06:40 +0530

    login api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,9 @@ import express from "express";
 
 const router = express.Router();
 //contollers
-import { register } from "../controllers/auth.js";
+import { register, login } from "../controllers/auth.js";
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
