b3e081f92d107ca3c85377d0ead1de2469a36aac
hem193 <hemanthcastel@gmail.com>
2023-01-29 23:16:53 +0530

    added dashboard-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
 import Menu from "./components/nav/Menu";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/routes/PrivateRoute";
 
 export default function App() {
   return (
@@ -15,6 +16,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
